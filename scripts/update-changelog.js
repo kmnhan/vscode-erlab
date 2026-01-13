22f308b32b67469a8c3fbe3294eb3d6629630b46
Kimoon Higashihira Han <khan@kaist.ac.kr>
2026-01-13 23:09:04 +0900

    chore: better version update scripts

---
+++ scripts/update-changelog.js
@@ -0,0 +1,25 @@
+// scripts/update-changelog.js
+// Automatically updates CHANGELOG.md during `npm version` to replace
+// ## [Unreleased] with ## [Unreleased]\n\n## [vX.X.X] - YYYY-MM-DD
+
+const fs = require("fs");
+const path = require("path");
+
+const pkgPath = path.join(__dirname, "..", "package.json");
+const changelogPath = path.join(__dirname, "..", "CHANGELOG.md");
+
+const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
+const changelog = fs.readFileSync(changelogPath, "utf8");
+
+const date = new Date().toISOString().split("T")[0];
+const newSection = `## [Unreleased]\n\n## [v${pkg.version}] - ${date}`;
+
+const updated = changelog.replace("## [Unreleased]", newSection);
+
+if (updated === changelog) {
+  console.error("Warning: Could not find '## [Unreleased]' in CHANGELOG.md");
+  process.exit(1);
+}
+
+fs.writeFileSync(changelogPath, updated);
+console.log(`Updated CHANGELOG.md with v${pkg.version}`);
